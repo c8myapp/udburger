6595ebc86d177ba9ab99cb0bddd95b8f0577f614
ERIK\Erik <ERIK77500@GMAIL.COM>
2018-11-29 23:31:26 +0700

    import PropTypes

--- src/components/burger/burgerIngredient/BurgerIngredient.jsx
+++ src/components/burger/burgerIngredient/BurgerIngredient.jsx
@@ -1,36 +1,45 @@
-import React from "react";
+import React, { Component } from 'react'
 import css from "./BurgerIngredient.css";
+import PropTypes from "prop-types";
 
-const BurgerIngredient = props => {
-  let ingredients = null;
-  switch (props.type) {
-    case "bread-bottom":
-      ingredients = <div className={css.BreadBottom} />;
-      break;
-    case "bread-top":
-      ingredients = (
-        <div className={css.BreadTop}>
-          <div className={css.Seeds1} />
-          <div className={css.Seeds2} />
-        </div>
-      );
-      break;
-    case "meat":
-      ingredients = <div className={css.Meat} />;
-      break;
-    case "cheese":
-      ingredients = <div className={css.Cheese} />;
-      break;
-    case "salad":
-      ingredients = <div className={css.Salad} />;
-      break;
-    case "bacon":
-      ingredients = <div className={css.Bacon} />;
-      break;
-    default:
-      ingredients = null;
+
+class BurgerIngredient extends Component {
+  render() {
+	let ingredients = null;
+	switch (this.props.type) {
+	  case "bread-bottom":
+		ingredients = <div className={css.BreadBottom} />;
+		break;
+	  case "bread-top":
+		ingredients = (
+		  <div className={css.BreadTop}>
+			<div className={css.Seeds1} />
+			<div className={css.Seeds2} />
+		  </div>
+		);
+		break;
+	  case "meat":
+		ingredients = <div className={css.Meat} />;
+		break;
+	  case "cheese":
+		ingredients = <div className={css.Cheese} />;
+		break;
+	  case "salad":
+		ingredients = <div className={css.Salad} />;
+		break;
+	  case "bacon":
+		ingredients = <div className={css.Bacon} />;
+		break;
+	  default:
+		ingredients = null;
+	}
+	return ingredients;
   }
-  return ingredients;
+}
+
+BurgerIngredient.propType=
+{
+	type:PropTypes.string.isRequired
 };
 
-export default BurgerIngredient;
+export default  BurgerIngredient;
