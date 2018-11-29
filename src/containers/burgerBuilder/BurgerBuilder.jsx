0586d2d566defe9b74b0fcc07a93b739efec203d
ERIK\Erik <ERIK77500@GMAIL.COM>
2018-11-29 17:31:26 +0700

    container for buger and build controls for ingredients

---
+++ src/containers/burgerBuilder/BurgerBuilder.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import ContainerChild from '../../hoc/ContainerChild';
+
+export default class BurgerBuilder extends Component {
+  render() {
+	return (
+	  <ContainerChild>
+		<div>Burger</div>
+		<div>Build Controls</div>
+	  </ContainerChild>
+		
+	  
+	)
+  }
+}
